bd3a2e45bb317e5bc0a33f5b33c6dfce90568b43
@yonathan.transky <yonathan.transky@peer39.com>
2024-04-05 11:49:41 +0300

    fix merge issues

--- src/macros.c
+++ src/macros.c
@@ -111,7 +111,6 @@ void _free_macro_lines(const KeyValuePair kvp)
         mb->lines_list = NULL;
     }
 }
-<<<<<<< HEAD
 
 /**
  * @brief Frees the global macro table and all associated resources.
@@ -120,20 +119,12 @@ void _free_macro_lines(const KeyValuePair kvp)
  * Then, it destroys the macro table itself.
  */
 void free_macro_table()
-=======
-void free_macro_table(void)
->>>>>>> 31a7347c0bac360fcdad3d812862453b5b11564d
 {
     if (macroTable == NULL)
         return;
 
-<<<<<<< HEAD
-    hashtable_iterate(macroTable, _free_macro_lines); /* Free the lines linked list for each macro. */
-    hashtable_destroy(macroTable); /* Free the macro table. */
-=======
     /* delete the lines linkedlist for each macro first*/
     hashtable_iterate(macroTable, _free_macro_lines);
     /* delete the macro hashtable */
     hashtable_destroy(macroTable);
->>>>>>> 31a7347c0bac360fcdad3d812862453b5b11564d
 }
